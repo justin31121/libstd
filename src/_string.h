2d107ad89202893e226f9f34b6ea47082441be31
Justin Schartner <justin.schartner@tu-dortmund.de>
2024-03-07 21:10:07 +0100

    Forgot string_u32

--- src/_string.h
+++ src/_string.h
@@ -1,12 +1,36 @@
 #ifndef _STRING_H
 #define _STRING_H
 
+// MIT License
+// 
+// Copyright (c) 2024 Justin Schartner
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 typedef unsigned char string_u8;
+typedef unsigned int string_u32;
 typedef int string_s32;
 typedef long long string_s64;
 typedef unsigned long long string_u64;
 
 #define u8 string_u8
+#define u32 string_u32
 #define s32 string_s32
 #define s64 string_s64
 #define u64 string_u64
@@ -467,6 +491,7 @@ STRING_DEF Rune rune_unescape(u8 **data, u64 *data_len) {
 #endif // STRING_IMPLEMENTATION
 
 #undef u8
+#undef u32
 #undef s32
 #undef s64
 #undef u64
